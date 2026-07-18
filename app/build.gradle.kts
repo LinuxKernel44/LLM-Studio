28aef330a63f29c33b04e0d0a1bd9dd2159c6e73
LinuxKernel44 <david.pallier.pro@gmail.com>
2026-07-18 19:47:40 +0200

    Fixed Whisper.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.linuxkernel44.llmstudio"
         minSdk = 27
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
